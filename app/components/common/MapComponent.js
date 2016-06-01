c9870379dbab9079077e876a206446271a73b6bc
Rohit Pujari <rohitspujari@gmail.com>
2016-05-31 20:12:14 -0500

    cleaned up map bubble

--- app/components/common/MapComponent.js
+++ app/components/common/MapComponent.js
@@ -15,22 +15,9 @@ import NavigationBar from 'react-native-navbar';
 import AppConfig from './AppConfig';
 import Button from '../common/button';
 
-var { width, height } = Dimensions.get('window');
-
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.1;
-const LONGITUDE_DELTA =0.1;
-//const LATITUDE_DELTA = 0.00922;
-//const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
-const SPACE = 0.01;
-
-
 export default class MapComponent extends Component {
   constructor(props) {
     super(props);
-
     //console.log('I am in MapComponent constructor');
   }
 
@@ -40,19 +27,46 @@ export default class MapComponent extends Component {
     if(this.props.currentMapArea) {
       this.props.currentMapArea(region);
     }
-
   }
-  //
-  // onRegionChangeComplete(region) {
-  //   this.setState({ region });
-  // }
+
+  getRating(rating){
+    return (
+      <View style={{width:20}}>
+       <StarRating
+       disabled={true}
+       emptyStar={'ios-star-outline'}
+       fullStar={'ios-star'}
+       halfStar={'ios-star-half'}
+       iconSet={'Ionicons'}
+       maxStars={5}
+       rating={rating}
+       starColor={AppConfig.themeStarColor()}
+       starSize={15}
+       selectedStar={() => {
+         return null;
+       }}
+     />
+     </View>
+    );
+  }
+
+  getDistance (lat1, lon1, lat2, lon2, unit) {
+  	var radlat1 = Math.PI * lat1/180
+  	var radlat2 = Math.PI * lat2/180
+  	var theta = lon1-lon2
+  	var radtheta = Math.PI * theta/180
+  	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  	dist = Math.acos(dist)
+  	dist = dist * 180/Math.PI
+  	dist = dist * 60 * 1.1515
+  	if (unit=="K") { dist = dist * 1.609344 }
+  	if (unit=="N") { dist = dist * 0.8684 }
+  	return dist
+  }
 
 
-
-  render(){
-
+  render() {
   //console.log('im rendering')
-
   const rightButtonConfig = {
     title: 'Close',
     tintColor:AppConfig.themeTextColor(),
@@ -64,8 +78,6 @@ export default class MapComponent extends Component {
     title: 'Map',
   };
 
-  //console.log(this.props.isChild);
-
   var navigationBar = null;
   if(!this.props.isChild){
     var navigationBar =  (<NavigationBar
@@ -75,26 +87,14 @@ export default class MapComponent extends Component {
        leftButton={<Button type="navBar" icon="mail-forward" onPress={() => this.props.navigator.pop()}/>}
        rightButton={<Button type="navBar" icon="close" onPress={() => this.props.navigator.pop()}/>} />);
   }
-
-  //console.log(navigationBar);
-
-   //console.log('I am in MapComponent render')
-   //console.log(coordinate);
-   //const { region, markers } = this.state;
-
-   //markers.push(this.props.location)
-   //console.log(markers);
-   return (
-
-     <View style={{flex:1}}>
+  return (
+    <View style={{flex:1}}>
      {navigationBar}
       <MapView  style={{flex:1, justifyContent:'center', alignItems:'center'}}
         region={this.props.region}
         onRegionChangeComplete={this.onRegionChange.bind(this)}
         showsUserLocation={true}
       >
-
-
       {this.props.markers.map(marker => (
         <MapView.Marker
           key={marker.longitude?marker.longitude: marker.place_id}
@@ -105,36 +105,86 @@ export default class MapComponent extends Component {
             longitude: marker.longitude?marker.longitude:marker.geometry.location.lng
           }}>
           <MapView.Callout>
-            <View>
-             <StarRating
-             disabled={true}
-             emptyStar={'ios-star-outline'}
-             fullStar={'ios-star'}
-             halfStar={'ios-star-half'}
-             iconSet={'Ionicons'}
-             maxStars={5}
-             rating={marker.rating?marker.rating:0}
-             starColor={'red'}
-             starSize={10}
-             selectedStar={() => {
-               return null;
-             }}
-           />
-           <Text>{marker.name?marker.name:this.props.place.name}</Text>
-           </View>
+            {this.getCalloutContent(marker)}
           </MapView.Callout>
         </MapView.Marker>
-
-
       ))}
-
       </MapView>
-      </View>
-
-
-
-
+    </View>
     )
   }
 
+  getCalloutContent(marker){
+
+    let {lat, lng} = marker.geometry.location;
+    let {latitude,longitude} = this.props.region;
+    let distance = Math.round(this.getDistance(latitude,longitude,lat,lng,'M')*10)/10;
+
+    var openNow = null;
+    if(marker.opening_hours){
+       console.log(marker.opening_hours);
+       if(marker.opening_hours.open_now === 'true') {
+         openNow = <Text style={{color:'green', fontSize:10}}>Open</Text>
+       } else {
+         openNow = <Text style={{color:'red', fontSize:10}}>Closed</Text>
+       }
+    }
+    //console.log(marker)
+    return (
+      <View style={styles.container}>
+        <View style={styles.detailsContainer}>
+        {this.getRating(marker.rating)}
+        <Text style={{fontWeight:'normal'}}>{marker.name?marker.name:this.props.place.name}</Text>
+        <Text style={{fontSize:12,color:'gray'}}>{marker.vicinity}</Text>
+        </View>
+        <View style={styles.attributesContainer}>
+         <View style={styles.openClosed}>
+          {openNow}
+         </View>
+         <View style={styles.distance}>
+          <Text style={{fontSize:10, borderWidth:0, color:'gray'}}>{distance+' mile'}</Text>
+         </View>
+
+
+
+        </View>
+      </View>
+    )
+  }
 }
+
+var styles = StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'row',
+    borderWidth:0,
+    justifyContent:'center',
+    //alignItems:'flex-start'
+
+  },
+  detailsContainer:{
+    flex:5,
+    justifyContent:'flex-end',
+    //alignItems:'flex-'
+    //borderWidth:1
+  },
+  attributesContainer: {
+    marginLeft:5,
+    flex:1,
+    //borderWidth:1
+
+  },
+  openClosed: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'flex-start',
+    borderWidth:0
+  },
+  distance: {
+    flex:1,
+    padding:1,
+    alignItems:'center',
+    justifyContent:'flex-end',
+    borderWidth:0
+  }
+});
